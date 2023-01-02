b0d2091b817be2b1b11e8dc475a5eecd0a2dc19d
Rhama Krisner <rhamakrisner@gmail.com>
2023-01-01 23:07:35 -0300

    Adicionado o setId. (Correção do bug de update e delete)

--- app/src/main/java/com/example/controledeprodutos/Produto.java
+++ app/src/main/java/com/example/controledeprodutos/Produto.java
@@ -28,6 +28,10 @@ public class Produto implements Serializable {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
